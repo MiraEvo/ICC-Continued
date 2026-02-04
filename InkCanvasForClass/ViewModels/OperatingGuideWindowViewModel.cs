8439da62002cfeac440aca2c623699082d854b28
SakuraStar <SakuraStar_CN@outlook.com>
2026-02-04 21:37:23 +0800

    refactor: 将 OperatingGuideWindow 重构为 MVVM 模式
    - 创建 OperatingGuideWindowViewModel，使用 CommunityToolkit.Mvvm 源生成器
    - 将窗口状态管理和全屏切换逻辑迁移到 ViewModel
    - 更新 XAML 绑定，使用数据驱动 UI
    - 精简代码后置，仅处理窗口生命周期和视图相关操作

---
+++ InkCanvasForClass/ViewModels/OperatingGuideWindowViewModel.cs
@@ -0,0 +1,89 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Ink_Canvas.Core;
+using System;
+
+namespace Ink_Canvas.ViewModels
+{
+    /// <summary>
+    /// OperatingGuideWindow ViewModel - 管理操作指南窗口状态和逻辑
+    /// </summary>
+    public partial class OperatingGuideWindowViewModel : ViewModelBase
+    {
+        #region 属性
+
+        /// <summary>
+        /// 窗口状态
+        /// </summary>
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNormalState))]
+        private System.Windows.WindowState _windowState = System.Windows.WindowState.Normal;
+
+        /// <summary>
+        /// 是否为正常状态（非最大化）
+        /// </summary>
+        public bool IsNormalState => WindowState == System.Windows.WindowState.Normal;
+
+        /// <summary>
+        /// 全屏按钮图标
+        /// </summary>
+        [ObservableProperty]
+        private string _fullscreenButtonIcon = "\uE740";
+
+        #endregion
+
+        #region 命令
+
+        /// <summary>
+        /// 关闭窗口命令
+        /// </summary>
+        [RelayCommand]
+        private void Close()
+        {
+            RequestClose?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// 切换全屏命令
+        /// </summary>
+        [RelayCommand]
+        private void ToggleFullscreen()
+        {
+            if (WindowState == System.Windows.WindowState.Normal)
+            {
+                WindowState = System.Windows.WindowState.Maximized;
+                FullscreenButtonIcon = "\uE73F";
+            }
+            else
+            {
+                WindowState = System.Windows.WindowState.Normal;
+                FullscreenButtonIcon = "\uE740";
+            }
+        }
+
+        /// <summary>
+        /// 窗口拖动命令
+        /// </summary>
+        [RelayCommand]
+        private void DragMove()
+        {
+            DragMoveRequested?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region 事件
+
+        /// <summary>
+        /// 请求关闭窗口事件
+        /// </summary>
+        public event EventHandler RequestClose;
+
+        /// <summary>
+        /// 请求拖动窗口事件
+        /// </summary>
+        public event EventHandler DragMoveRequested;
+
+        #endregion
+    }
+}
--- InkCanvasForClass/Windows/OperatingGuideWindow.xaml.cs
+++ InkCanvasForClass/Windows/OperatingGuideWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Ink_Canvas.Helpers;
+using Ink_Canvas.ViewModels;
+using System;
 using System.Windows;
 using System.Windows.Input;
 
@@ -9,34 +11,79 @@ namespace Ink_Canvas.Dialogs
     /// </summary>
     public partial class OperatingGuideWindow : Window
     {
+        /// <summary>
+        /// ViewModel 实例
+        /// </summary>
+        public OperatingGuideWindowViewModel ViewModel { get; private set; }
+
         public OperatingGuideWindow()
         {
             InitializeComponent();
+
+            // 创建并设置 ViewModel
+            ViewModel = new OperatingGuideWindowViewModel();
+            DataContext = ViewModel;
+
+            // 订阅 ViewModel 事件
+            ViewModel.RequestClose += OnRequestClose;
+            ViewModel.DragMoveRequested += OnDragMoveRequested;
+
             AnimationsHelper.ShowWithSlideFromBottomAndFade(this, 0.25);
         }
 
-        private void BtnClose_MouseUp(object sender, MouseButtonEventArgs e)
+        /// <summary>
+        /// 请求关闭窗口
+        /// </summary>
+        private void OnRequestClose(object sender, EventArgs e)
         {
             Close();
         }
 
-        private void WindowDragMove(object sender, MouseEventArgs e)
+        /// <summary>
+        /// 请求拖动窗口
+        /// </summary>
+        private void OnDragMoveRequested(object sender, EventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+            DragMove();
         }
 
-        private void BtnFullscreen_MouseUp(object sender, MouseButtonEventArgs e) {
-            if (WindowState == WindowState.Normal) {
-                WindowState = WindowState.Maximized;
-                SymbolIconFullscreen.Glyph = "\uE73F";
-            } else {
-                WindowState = WindowState.Normal;
-                SymbolIconFullscreen.Glyph = "\uE740";
+        private void BtnClose_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ViewModel?.CloseCommand.Execute(null);
+        }
+
+        private void WindowDragMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                ViewModel?.DragMoveCommand.Execute(null);
             }
         }
 
-        private void ScrollViewerManipulationBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e) {
+        private void BtnFullscreen_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ViewModel?.ToggleFullscreenCommand.Execute(null);
+        }
+
+        private void ScrollViewerManipulationBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e)
+        {
             e.Handled = true;
+        }
+
+        /// <summary>
+        /// 窗口关闭时清理资源
+        /// </summary>
+        protected override void OnClosed(EventArgs e)
+        {
+            // 取消订阅事件
+            if (ViewModel != null)
+            {
+                ViewModel.RequestClose -= OnRequestClose;
+                ViewModel.DragMoveRequested -= OnDragMoveRequested;
+                ViewModel.Cleanup();
+            }
+
+            base.OnClosed(e);
         }
     }
 }
